735a4b8735b328e7e1440e77eaca1b62f6c3ce29
Nikita <sorokinn1398@gmail.com>
2021-10-27 13:35:43 +0400

    Add output managment

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
     mode: "none",
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({})
+    ],
     output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, 'dist')
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
     }
 }
